d2a02d3e67888373e83c36382494faec365bd681
Jaylan <jaylancharles.autumn@gmail.com>
2026-05-26 02:54:16 +0800

    feat(user):初始化数据库进一步实现signup流程并重构项目结构 (#3)

---
+++ internal/repository/user.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/JaylanCharles/byline/internal/domain"
+	"github.com/JaylanCharles/byline/internal/repository/dao"
+)
+
+type UserRepository struct {
+	dao *dao.UserDAO
+}
+
+func NewUserRepository(dao *dao.UserDAO) *UserRepository {
+	return &UserRepository{
+		dao: dao,
+	}
+}
+func (r *UserRepository) Create(ctx context.Context, u domain.User) error {
+	return r.dao.Insert(ctx, dao.User{
+		Email:    u.Email,
+		Password: u.Password,
+	})
+}
+func (r *UserRepository) FindById(int64) {
+	// 先从 cache 里面找
+	// 再从 dao 里面找
+	// 找到了回写 cache
+}
--- main.go
+++ main.go
@@ -4,14 +4,28 @@ import (
 	"strings"
 	"time"
 
+	"github.com/JaylanCharles/byline/internal/repository"
+	"github.com/JaylanCharles/byline/internal/repository/dao"
+	"github.com/JaylanCharles/byline/internal/service"
 	"github.com/JaylanCharles/byline/internal/web"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	server := gin.Default()
+	db := InitDB()
+	
+	server := InitWebServer()
+	u := InitUser(db)
+	u.RegisterRoutes(server)
 
+	server.Run(":8080")
+}
+
+func InitWebServer() *gin.Engine {
+	server := gin.Default()
 	// 作用于定义在server上的全部路由
 	// 怎么改？就看前端页面的请求对照着改
 	server.Use(cors.New(cors.Config{
@@ -34,10 +48,30 @@ func main() {
 		},
 		MaxAge: 12 * time.Hour,
 	}))
+	return server
+}
 
+func InitUser(db *gorm.DB) *web.UserHandler {
+	ud := dao.NewUserDAO(db)
+	repo := repository.NewUserRepository(ud)
+	svc := service.NewUserService(repo)
 	//u := &web.UserHandler{}
-	u := web.NewUserHandler()
-	u.RegisterRoutes(server)
+	u := web.NewUserHandler(svc)
+	return u
+}
 
-	server.Run(":8080")
+func InitDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:13316)/byline"))
+	// 只会在初始化过程中使用 panic
+	// panic 相当于整个 goroutine 结束
+	// 一旦初始化过程出错，应用就不要启动了
+	if err != nil {
+		panic(err)
+	}
+
+	err = dao.InitTable(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
