f4ecc0411cf532fdff24df5619b001e9c5706775
Jaylan <jaylancharles.autumn@gmail.com>
2026-06-01 13:44:50 +0800

    jwt-improve

--- internal/web/middleware/login_jwt.go
+++ internal/web/middleware/login_jwt.go
@@ -1,9 +1,12 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/JaylanCharles/byline/internal/web"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -43,7 +46,9 @@ func (l *LoginJWTMiddlewareBuilder) Build() gin.HandlerFunc {
 			return
 		}
 		tokenStr := segs[1]
-		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (any, error) {
+		// 这里使用指针的原因是， jwt.ParseWithClaims 中会获取 claims 将他赋值带出来
+		claims := &web.UserClaims{}
+		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
 			return []byte("vjYqKKBpPfsWGpfq1Ljo57BgjsMg9yBr"), nil
 		})
 		if err != nil {
@@ -57,5 +62,19 @@ func (l *LoginJWTMiddlewareBuilder) Build() gin.HandlerFunc {
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
+
+		now := time.Now()
+		// 每十秒钟刷新一次 (就是重新生成了一个token)
+		if claims.ExpiresAt.Sub(now) < time.Second*50 {
+			claims.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute))
+			tokenStr, err = token.SignedString([]byte("vjYqKKBpPfsWGpfq1Ljo57BgjsMg9yBr"))
+			if err != nil {
+				// 记录日志
+				log.Println("jwt 续约失败", err)
+			}
+			ctx.Header("x-jwt-token", tokenStr) // 重新返回给前端
+		}
+
+		ctx.Set("claims", claims)
 	}
 }
