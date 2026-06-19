ccd4f5d61b219190d588eddc64b8708422639fb9
Jaylan <jaylancharles.autumn@gmail.com>
2026-06-20 06:33:22 +0800

    feat(viper,etcd): 引入 viper 和 etcd，实现 viper 两个策略 (#34)

--- ioc/db.go
+++ ioc/db.go
@@ -1,14 +1,25 @@
 package ioc
 
 import (
-	"github.com/JaylanCharles/byline/config"
 	"github.com/JaylanCharles/byline/internal/repository/dao"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+	type Config struct {
+		DSN string `yaml:"dsn"`
+	}
+	var cfg = Config{
+		DSN: "root:root@tcp(localhost:13316)/byline_default",
+	}
+	err := viper.UnmarshalKey("db", &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(mysql.Open(cfg.DSN))
 	// 只会在初始化过程中使用 panic
 	// panic 相当于整个 goroutine 结束
 	// 一旦初始化过程出错，应用就不要启动了
--- main.go
+++ main.go
@@ -1,6 +1,44 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
+)
+
 func main() {
+	initViper()
 	server := InitWebServer()
 	server.Run(":8080")
+}
+
+func initViperRemote() {
+	err := viper.AddRemoteProvider("etcd3", "http://127.0.0.1:12379", "/byline")
+	if err != nil {
+		panic(err)
+	}
+	viper.SetConfigType("yaml")
+	err = viper.ReadRemoteConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initViper() {
+	cfile := pflag.String("config", "config/config.yaml", "指定配置文件路径")
+	pflag.Parse()
+	viper.SetConfigFile(*cfile)
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println(in.Name, in.Op)
+		// 重新读新配置
+	})
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
