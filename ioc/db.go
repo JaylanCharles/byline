5278364e97f9eaaf8d84710247d3673ca1854b02
Jaylan <jaylancharles.autumn@gmail.com>
2026-06-20 20:04:45 +0800

    Feature/log (#37)
    * fix(redis-jwt): CheckSession 勘误
    
    * feat(init): 初始化 zap，使用全局 logger，预留 user 独立 logger
    
    * refactor(zap): 使用适配器模式，抽象 Logger 接口
    
    * feat(ginx): 设计 gin middleware
    
    * fix(ioc): debug
    
    * feat(viper): 实现动态监听
    
    * feat(gorm): 利用 gorm 实现 db 日志打印

--- ioc/db.go
+++ ioc/db.go
@@ -1,13 +1,17 @@
 package ioc
 
 import (
+	"time"
+
 	"github.com/JaylanCharles/byline/internal/repository/dao"
+	"github.com/JaylanCharles/byline/pkg/logger"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	glogger "gorm.io/gorm/logger"
 )
 
-func InitDB() *gorm.DB {
+func InitDB(l logger.Logger) *gorm.DB {
 	type Config struct {
 		DSN string `yaml:"dsn"`
 	}
@@ -19,7 +23,14 @@ func InitDB() *gorm.DB {
 		panic(err)
 	}
 
-	db, err := gorm.Open(mysql.Open(cfg.DSN))
+	db, err := gorm.Open(mysql.Open(cfg.DSN), &gorm.Config{
+		Logger: glogger.New(gormLoggerFunc(l.Debug), glogger.Config{
+			SlowThreshold:             time.Millisecond * 10,
+			IgnoreRecordNotFoundError: true, // 有的人就是不关心
+			ParameterizedQueries:      true, // 让 value 全变成 ？
+			LogLevel:                  glogger.Info,
+		}),
+	})
 	// 只会在初始化过程中使用 panic
 	// panic 相当于整个 goroutine 结束
 	// 一旦初始化过程出错，应用就不要启动了
@@ -32,4 +43,13 @@ func InitDB() *gorm.DB {
 		panic(err)
 	}
 	return db
+}
+
+type gormLoggerFunc func(msg string, fields ...logger.Field)
+
+func (g gormLoggerFunc) Printf(msg string, args ...interface{}) {
+	g(msg, logger.Field{
+		Key:   "args",
+		Value: args,
+	})
 }
