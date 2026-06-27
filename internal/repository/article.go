3ed98edbbb1263cc86c48c27198419c7e888c73b
Jaylan <jaylancharles.autumn@gmail.com>
2026-06-27 08:40:50 +0800

    设计缓存

--- internal/repository/article.go
+++ internal/repository/article.go
@@ -5,42 +5,90 @@ import (
 	"time"
 
 	"github.com/JaylanCharles/byline/internal/domain"
+	"github.com/JaylanCharles/byline/internal/repository/cache"
 	dao "github.com/JaylanCharles/byline/internal/repository/dao/article"
+	"github.com/JaylanCharles/byline/pkg/logger"
 	"github.com/ecodeclub/ekit/slice"
 )
 
 type ArticleRepository interface {
 	Create(ctx context.Context, art domain.Article) (int64, error)
 	Update(ctx context.Context, art domain.Article) error
+
 	Sync(ctx context.Context, art domain.Article) (int64, error)
 	SyncStatus(ctx context.Context, id int64, author int64, status domain.ArticleStatus) error
+
 	List(ctx context.Context, uid int64, offset int, limit int) ([]domain.Article, error)
 }
 
 type CachedArticleRepository struct {
-	dao dao.ArticleDAO
+	dao   dao.ArticleDAO
+	cache cache.ArticleCache
+	l     logger.Logger
 }
 
-func NewCachedArticleRepository(dao dao.ArticleDAO) ArticleRepository {
+func NewCachedArticleRepository(dao dao.ArticleDAO, l logger.Logger) ArticleRepository {
 	return &CachedArticleRepository{
 		dao: dao,
+		l:   l,
 	}
+}
+
+func (c *CachedArticleRepository) Create(ctx context.Context, art domain.Article) (int64, error) {
+	defer func() {
+		// 清空缓存
+		_ = c.cache.DelFirstPage(ctx, art.Author.Id)
+	}()
+	return c.dao.Insert(ctx, c.toEntity(art))
+}
+
+func (c *CachedArticleRepository) Update(ctx context.Context, art domain.Article) error {
+	defer func() {
+		// 清空缓存
+		_ = c.cache.DelFirstPage(ctx, art.Author.Id)
+	}()
+	return c.dao.UpdateById(ctx, c.toEntity(art))
+}
+
+func (c *CachedArticleRepository) Sync(ctx context.Context, art domain.Article) (int64, error) {
+	defer func() {
+		// 清空缓存
+		_ = c.cache.DelFirstPage(ctx, art.Author.Id)
+	}()
+	return c.dao.Sync(ctx, c.toEntity(art))
 }
 
 func (c *CachedArticleRepository) SyncStatus(ctx context.Context, id int64, author int64, status domain.ArticleStatus) error {
 	return c.dao.SyncStatus(ctx, id, author, status.ToUint8())
 }
 
-func (c *CachedArticleRepository) Sync(ctx context.Context, art domain.Article) (int64, error) {
-	return c.dao.Sync(ctx, c.toEntity(art))
-}
-
-func (c *CachedArticleRepository) Create(ctx context.Context, art domain.Article) (int64, error) {
-	return c.dao.Insert(ctx, c.toEntity(art))
-}
-
-func (c *CachedArticleRepository) Update(ctx context.Context, art domain.Article) error {
-	return c.dao.UpdateById(ctx, c.toEntity(art))
+func (c *CachedArticleRepository) List(ctx context.Context, uid int64, offset int, limit int) ([]domain.Article, error) {
+	// 你在这个地方，集成你的复杂的缓存方案
+	// 你只缓存这一页
+	if offset == 0 && limit <= 100 {
+		data, err := c.cache.GetFirstPage(ctx, uid)
+		if err == nil {
+			go func() {
+				c.preCache(ctx, data)
+			}()
+			//return data[:limit], err
+			return data, err
+		}
+	}
+	res, err := c.dao.GetByAuthor(ctx, uid, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+	data := slice.Map[dao.Article, domain.Article](res, func(idx int, src dao.Article) domain.Article {
+		return c.toDomain(src)
+	})
+	// 回写缓存的时候，可以同步，也可以异步
+	go func() {
+		err := c.cache.SetFirstPage(ctx, uid, data)
+		c.l.Error("回写缓存失败", logger.Error(err))
+		c.preCache(ctx, data)
+	}()
+	return data, nil
 }
 
 func (c *CachedArticleRepository) toEntity(art domain.Article) dao.Article {
@@ -67,13 +115,11 @@ func (c *CachedArticleRepository) toDomain(art dao.Article) domain.Article {
 	}
 }
 
-func (c *CachedArticleRepository) List(ctx context.Context, uid int64, offset int, limit int) ([]domain.Article, error) {
-	res, err := c.dao.GetByAuthor(ctx, uid, offset, limit)
-	if err != nil {
-		return nil, err
+func (c *CachedArticleRepository) preCache(ctx context.Context, data []domain.Article) {
+	if len(data) > 0 && len(data[0].Content) < 1024*1024 {
+		err := c.cache.Set(ctx, data[0])
+		if err != nil {
+			c.l.Error("提前预加载缓存失败", logger.Error(err))
+		}
 	}
-
-	return slice.Map[dao.Article, domain.Article](res, func(idx int, src dao.Article) domain.Article {
-		return c.toDomain(src)
-	}), nil
 }
